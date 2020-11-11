c22f075af49a352c76b9aaa3388bf2c7129fecc3
FilipTrajkovski <filip_trajkovski94@hotmail.com>
2020-11-11 19:49:25 +0100

    Added spring boot main app file and the servlet initializer - Added the application properties with the initial setup of the app - Added gitignore for the api module

---
+++ api/src/main/java/mk/ukim/finki/dians/api/NewYorkBusStationsApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dians.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYorkBusStationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewYorkBusStationsApplication.class, args);
+	}
+
+}
